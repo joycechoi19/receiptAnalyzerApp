e8f3f02d99a3aa5215ba746914bff3fd9ef131c0
Helen Lee <hl148@duke.edu>
2017-04-21 14:16:22 -0400

    started integrating OCR reader into master branch

---
+++ and-nd-firebase/app/src/main/java/com/google/firebase/udacity/receiptapp/InfoEditorActivity.java
@@ -0,0 +1,38 @@
+package com.google.firebase.udacity.receiptapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+
+public class InfoEditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checker);
+    }
+
+//    public ArrayList<String> getReceipt(ArrayList<String> receipt){
+//        Intent i = new Intent(OcrCaptureActivity.this, InfoEditorActivity.class);
+//                startActivity(i);
+//
+//        ArrayList<String> info = OcrCaptureActivity.getReceipt();
+//        return info;
+//    }
+
+
+    public String getStoreName(ArrayList<String> info){
+        String storeName = info.get(0);
+        return storeName;
+    }
+    public String getAddress(ArrayList<String> info){
+        String address = info.get(1);
+        return address;
+    }
+    public String getPrice(ArrayList<String> info){
+        String price = info.get(2);
+        return price;
+    }
+
+}
